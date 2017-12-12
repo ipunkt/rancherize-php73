26f22a210d8fa4f470cd9195173cf704ac7df308
Sven Speckmaier <sps@ipunkt.biz>
2017-12-12 12:09:23 +0100

    Register Php72 with the PhpFpmMaker

--- plugin/Php70Provider.php
+++ plugin/Php70Provider.php
@@ -1,7 +1,9 @@
 <?php namespace RancherizePhp70;
 
+use Rancherize\Blueprint\Infrastructure\Service\Maker\PhpFpm\PhpFpmMaker;
 use Rancherize\Plugin\Provider;
 use Rancherize\Plugin\ProviderTrait;
+use RancherizePhp70\PhpVersion\Php72;
 
 /**
  * Class Php70Provider
@@ -14,10 +16,19 @@ class Php70Provider implements Provider {
 	/**
 	 */
 	public function register() {
+		$this->container[Php72::class] = function() {
+			return new Php72();
+		};
 	}
 
 	/**
 	 */
 	public function boot() {
+		/**
+		 * @var PhpFpmMaker $fpmMaker
+		 */
+		$fpmMaker = $this->container[PhpFpmMaker::class];
+
+		$fpmMaker->addVersion( $this->container[Php72::class] );
 	}
 }
