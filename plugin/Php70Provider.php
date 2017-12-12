0a366c5e28c43943cb0a0e62bae4c91eb21e9b4a
Sven Speckmaier <sps@ipunkt.biz>
2017-12-12 12:07:14 +0100

    php72 copied from php70 and changed image version

---
+++ plugin/Php70Provider.php
@@ -0,0 +1,23 @@
+<?php namespace RancherizePhp70;
+
+use Rancherize\Plugin\Provider;
+use Rancherize\Plugin\ProviderTrait;
+
+/**
+ * Class Php70Provider
+ * @package RancherizePhp70
+ */
+class Php70Provider implements Provider {
+
+	use ProviderTrait;
+
+	/**
+	 */
+	public function register() {
+	}
+
+	/**
+	 */
+	public function boot() {
+	}
+}
